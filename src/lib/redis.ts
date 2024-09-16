5bc1a2462958a35ee330d499518d7e18f6204606
Antoine BATTAGLIOTTI <antoinebattagliotti@outlook.com>
2024-09-17 00:13:58 +0200

    update ts config and package json to use top level await (for Redis)

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,15 +2,13 @@ import { createClient } from 'redis'
 
 const redisClient = createClient()
 
-;(async () => {
-    redisClient.on('error', (err) => console.log('Redis client error', err))
+redisClient.on('error', (err) => console.log('Redis client error', err))
 
-    redisClient.on('ready', () => console.log('Redis client started'))
+redisClient.on('ready', () => console.log('Redis client started'))
 
-    await redisClient.connect()
+await redisClient.connect()
 
-    await redisClient.ping()
-})()
+await redisClient.ping()
 
 // Make sure we are connected to that Redis client that we have defined it
 // Use an inline function, set to be async and add brackets to make sure the function runs immediately
